67a8a7d954fb670938fbc6b88b5e2bf7cb5bfcb4
Belen <mariabelenhb@gmail.com>
2015-10-02 17:47:52 -0600

    Version final comentada

--- Hoja8/src/hoja8/Hoja8.java
+++ Hoja8/src/hoja8/Hoja8.java
@@ -1,5 +1,4 @@
 package hoja8;
-import java.util.Scanner;
 import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,43 +11,64 @@ import java.util.PriorityQueue;
 public class Hoja8{
 
 public static void main(String[] args) {
+        //se crea un fichero
         File pacientes = new File("pacientes.txt");
+        //para la implementacion con heap
 	VectorHeap hospital;
+        hospital = new VectorHeap();
+        //para la implementacion con JCF
+        PriorityQueue<Paciente> hospital2;
+        hospital2= new PriorityQueue();
+        //instancia para la ficha del paciente
 	Paciente paciente;
-	String [] palabra= new String [3];
+        //informacion del paciente
+	String [] informacion;
+        //contadores
 	int num;
-	int num2;
+        int num2;
         int num3;
         num=0;
 	num2=1;
-	hospital = new VectorHeap();
-        PriorityQueue<Paciente> hospital2;
-        hospital2= new PriorityQueue();
+        
+        //inicio de la interaccion con el usuario
 	System.out.println("BIENVENIDOS AL HOSPITAL");
-	System.out.println("Atender a los pacientes en el siguiente orden: \n");
+	System.out.println("Orden de llegada de los pacientes:");
+        //se lee el archivo de texto con la informacion de los pacientes que van llegando
         try{
             BufferedReader br = new BufferedReader(new FileReader(pacientes));
             try { //array de tipo numeral que implementa comparable
                 int z=0;
                 for(String line; (line = br.readLine()) != null; ){
+                    System.out.println(num2+". "+line);
                     num= num +1;
-                    palabra= line.split(",");
-                    paciente= new Paciente(palabra[0],palabra[1],palabra[2]);
-                    hospital.add((Comparable) paciente);
-                    hospital2.add(paciente);
+                    //se guarda por separado la informacion del paciente
+                    informacion= line.split(",");
+                    //con la informacion se crea una ficha para el paciente
+                    paciente= new Paciente(informacion[0],informacion[1],informacion[2]);
+                    //se realizan las 2 diferenres implementaciones
+                    hospital.add((Comparable) paciente); //heap
+                    hospital2.add(paciente); //JCF
                     z++;
                 }
             }
             catch(IOException ex) {
                 Logger.getLogger(Hoja8.class.getName()).log(Level.SEVERE, null, ex);
             }
+            //Se imprimen los resultados, primero empleando heap y luego JCF
+            //los resultados deben ser los mismos
+            System.out.println("\nAtender a los pacientes en el siguiente orden: ");
+            
+            //implementacion con heap
             num3=num;
-            System.out.println("--Implementacion empleando un heap--");
+            num2=1;
+            System.out.println("\n--Implementacion empleando un heap--");
             while(num !=0){
                 System.out.println(num2+". " + hospital.remove().toString());
 		num = num-1;
                 num2++;
             }
+            
+            //implementacion con JCF
             num2=1;
             System.out.println("\n--Implementacion empleando JCF--");
             while(num3 !=0){
